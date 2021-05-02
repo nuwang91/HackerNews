a5ded2e35ef9e2960a0a12577e5b4416ff0ada22
Nuwan Gunaratne <nuwan.gunaratne@ifsworld.com>
2021-05-02 13:32:09 +0530

    feat(news): get news stories from rest api

--- src/app/shell/page/news/news.component.ts
+++ src/app/shell/page/news/news.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { switchMap, take } from 'rxjs/operators';
+import { DataProviderService } from 'src/app/services/data-provider.service';
 
 type NewsType = "job" | "story" | "comment" | "poll" | "pollopt";
 
@@ -29,19 +33,16 @@ export class NewsComponent implements OnInit {
   newsItems: Array<NewsItem> = [];
   dummyBody: string = 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, …when an unknown printer took a galley of type and scrambled';
 
-  constructor() {
+  private _mainUrlType: string = '';
+  private _top = 10;
+
+  constructor(
+    private dataProviderService: DataProviderService,
+    private route: ActivatedRoute,) {
   }
 
   ngOnInit(): void {
-    let testItem: any = {
-      time: 213131432,
-      id: 3421432,
-      descendants: 21,
-      text: 'Testing',
-      title: 'test title',
-      url: 'www.google.com'
-    };
-    this.newsItems.push(<any>testItem)
+    this._listenToRouteChanges();
   }
 
   getBodyText(text: string): string {
@@ -60,6 +61,39 @@ export class NewsComponent implements OnInit {
 
   getTime(time: number): string {
     return time.toString();
+  }
+
+  private _listenToRouteChanges(): void {
+    this.route.params.subscribe((params) => {
+      this.newsItems = [];
+      this.setNewsType(params.type);
+      this.getItems();
+    });
+  }
+
+  private setNewsType(type: string): void {
+    switch (type) {
+      case 'new':
+        this._mainUrlType = 'newstories';
+        break;
+      case 'best':
+        this._mainUrlType = 'beststories';
+        break;
+      default:
+        this._mainUrlType = 'topstories';
+        break;
+    }
+  }
+
+  private getItems(): void {
+    this.dataProviderService.getData(this._mainUrlType, `?orderBy="$key"&limitToFirst=${this._top}`)
+      .pipe(
+        switchMap((ids) => forkJoin(ids.map((id: number) => {
+          return this.dataProviderService.getData('item/' + id).pipe(take(1));
+        })))
+      ).pipe(take(1)).subscribe((items: any) => {
+        this.newsItems = items;
+      });
   }
 
 }
