454dd619b7ba873bc7d2d4807abc3e5871fdca69
Nuwan Gunaratne <nuwan.gunaratne@ifsworld.com>
2021-05-02 12:28:15 +0530

    feat(shell): use nav-item component in shell and include nav element with anchor element

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShellComponent } from './shell.component';
+import { NavItemModule } from './ui-components/nav-item/nav-item.module';
 
 @NgModule({
   declarations: [ShellComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NavItemModule
   ],
   exports: [ShellComponent]
 })
